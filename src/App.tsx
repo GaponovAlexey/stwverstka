603328f1735bd6030838cc2038a093fe666eeb35
Kekelberi <alexei.gaponov@gmail.com>
2021-12-30 22:27:38 +0300

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from 'react'
+import logo from './img/odigo-logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      start
-    </div>
-  );
+    <header className='header'>
+      <div className='wrapper'>
+        {/* logo */}
+        <div className='header__wrapper'>
+          <div className='header__logo'>
+            <a href='/' className='header__logo-link'>
+              <img src={logo} alt='Discover Amazing places in Japan' />
+            </a>
+          </div>
+          {/* nav */}
+          <nav className='header__nav'>
+            <ul className='header__list'>
+              <li className='header__item'>
+                <a href='=!' className='header__link'>
+                  Articles
+                </a>
+              </li>
+              <li className='header__item'>
+                <a href='=!' className='header__link'>
+                  Locations
+                </a>
+              </li>
+              <li className='header__item'>
+                <a href='=!' className='header__link'>
+                  Videos
+                </a>
+              </li>
+              <li className='header__item'>
+                <a href='=!' className='header__link'>
+                  Sign in
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
 }
 
-export default App;
+export default App
