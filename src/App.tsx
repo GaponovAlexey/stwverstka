fa442b3315d22001757aaaf8146208c18ffe488f
Kekelberi <alexei.gaponov@gmail.com>
2022-01-02 18:49:13 +0300

    save

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,48 @@ import logo from './img/odigo-logo.svg'
 
 function App() {
   return (
-    <header className='header'>
-      <div className='wrapper'>
-        {/* logo */}
-        <div className='header__wrapper'>
-          <div className='header__logo'>
-            <a href='/' className='header__logo-link'>
-              <img src={logo} alt='Discover Amazing places in Japan' />
-            </a>
+    <>
+      <header className='header'>
+        <div className='wrapper'>
+          <div className='header__wrapper'>
+            {/* logo */}
+            <div className='header__logo'>
+              <a href='/' className='header__logo-link'>
+                <img src={logo} alt='Discover Amazing places in Japan' />
+              </a>
+            </div>
+            {/* nav */}
+            <nav className='header__nav'>
+              <ul className='header__list'>
+                <li className='header__item'>
+                  <a href='=!' className='header__link'>
+                    Articles
+                  </a>
+                </li>
+                <li className='header__item'>
+                  <a href='=!' className='header__link'>
+                    Locations
+                  </a>
+                </li>
+                <li className='header__item'>
+                  <a href='=!' className='header__link'>
+                    Videos
+                  </a>
+                </li>
+                <li className='header__item'>
+                  <a href='=!' className='header__link'>
+                    Sign in
+                  </a>
+                </li>
+              </ul>
+            </nav>
           </div>
-          {/* nav */}
-          <nav className='header__nav'>
-            <ul className='header__list'>
-              <li className='header__item'>
-                <a href='=!' className='header__link'>
-                  Articles
-                </a>
-              </li>
-              <li className='header__item'>
-                <a href='=!' className='header__link'>
-                  Locations
-                </a>
-              </li>
-              <li className='header__item'>
-                <a href='=!' className='header__link'>
-                  Videos
-                </a>
-              </li>
-              <li className='header__item'>
-                <a href='=!' className='header__link'>
-                  Sign in
-                </a>
-              </li>
-            </ul>
-          </nav>
         </div>
-      </div>
-    </header>
+      </header>
+      <main className='main'>
+        <section className='intro'>da</section>
+      </main>
+    </>
   )
 }
 
