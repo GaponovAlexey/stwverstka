0dceb21496537455299a5be3570cf25c23928ff4
Kekelberi <alexei.gaponov@gmail.com>
2022-01-03 18:00:33 +0300

    win

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -4,8 +4,24 @@ export const Main = () => {
   return (
     <div>
       <main className='main'>
-        <section className='intro'>da</section>
-        
+        <section className='intro'>
+          <div className='wrapper'>
+            <h1 className='intro__title'>Discover Amazing places in Japan</h1>
+            <p className='intro__subtitle'>
+              Jump off balcony, onto stranger's head. Chase ball of string hide
+              when guests come over. Being gorgeous with belly side up i could
+              pee on this if i had the energy but under the bed, for attack the
+              child, open the door,
+            </p>
+            <form className='search__form'>
+              <p className='form__info'>
+                <input type='text' className='search__form-field' placeholder='What would you like to do?' />
+                <input type='text' className='search__form-field' placeholder='Where would you like to go?' />
+                <button className='form__submit'>search</button>
+              </p>
+            </form>
+          </div>
+        </section>
       </main>
     </div>
   )
